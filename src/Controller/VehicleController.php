384fa2b59cc74893bee2e413529766837b6fba86
Sebastien <sebguy.hierso@gmail.com>
2019-10-03 11:10:07 +0200

    fusion back-end

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -1,33 +1,33 @@
 <?php
-
 namespace App\Controller;
 use App\Entity\Vehicle;
 use App\Form\VehicleType;
 use App\Repository\VehicleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/vehicle", name="vehicle")
- */
 class VehicleController extends AbstractController
 {
     /**
-     * @Route("/add", name="_add")
+     * @param Request $request
+     * @return Response
+     * @Route(path="/vehicle",name="vehicle_app")
      */
-    public function add(Request $request): Response
+    public function vehicle(Request $request): Response
     {
         $isOk = false;
-        /** @var Vehicle $Vehicle */
+        /** @var Vehicle $vehicle */
         $vehicle = new Vehicle();
         $newVehicleForm = $this->createForm(VehicleType::class, $vehicle);
         $newVehicleForm->handleRequest($request);
         if ($newVehicleForm->isSubmitted() && $newVehicleForm->isValid()) {
+        
             $em = $this->getDoctrine()->getManager();
             $vehicle = $newVehicleForm->getData();
             $vehicle->setAvailable(1);
+            $vehicle->setPurchaseDate(new \DateTime());
             $em->persist($vehicle);
             $em->flush();
             $isOk = true;
@@ -38,3 +38,4 @@ class VehicleController extends AbstractController
         ]);
     }
 }
+
