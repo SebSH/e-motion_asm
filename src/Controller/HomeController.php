15696c1ef758169d096cc86b4cef873686991e18
Sebastien <sebguy.hierso@gmail.com>
2019-10-04 10:57:25 +0200

    ajout du front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+class HomeController extends abstractController
+{
 /** 
 * @Route("/", name="website_index")
 */ 
-class HomeController extends abstractController
-{
     public function index() {
         
         return $this->render('home/home.html.twig');
